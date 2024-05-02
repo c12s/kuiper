5c8b178d1df376572e81fe8cd3c27ee7e89e6053
Tamara Ranković <rankovictamaraa@gmail.com>
2024-05-02 12:41:26 +0200

    removed etcd run script

--- run-etcd.sh
+++
@@ -1,6 +0,0 @@
-docker run --name etcd-server \
-    --publish 2379:2379 \
-    --publish 2380:2380 \
-    --env ALLOW_NONE_AUTHENTICATION=yes \
-    --env ETCD_ADVERTISE_CLIENT_URLS=http://etcd-server:2379 \
-    bitnami/etcd:latest
